f202d6b36c17cd2f569cd5d3bd1265aaccf21bf8
Mark Lohstroh <mlohstroh@gmail.com>
2015-02-11 23:35:16 -0600

    full battle sequence

--- packages/custom/battle/public/controllers/battle.js
+++ packages/custom/battle/public/controllers/battle.js
@@ -12,7 +12,23 @@ angular.module('mean.battle')
         'failure': false
       };
 
+      $scope.battleState = {
+        'question' : true,
+        'postAnswer' : false,
+        'done': false
+      };
+
       $scope.activityLog = [];
+
+      $scope.monster = {
+        // these will be filled in
+        health: 0
+      };
+
+      $scope.player = {
+        currentHealth: Global.user.maxHealth, // set at max for now
+        power: 40 // I'm not sure what the spec is for power at the moment
+      };
 
       $scope.addActivityLog = function(item) {
         $scope.activityLog.push(item);
@@ -22,29 +38,44 @@ angular.module('mean.battle')
         $scope.difficulty = diff;
         stateMachine.switchState($scope.state, 'battle');
         $scope.addActivityLog('You choose to fight a ' + diff + ' monster');
-        $scope.startBattle(); // this is wrong, but it works for now. A controller shouldn't depend on a child.
+        $scope.startBattle();
+      };
+
+      // ML TODO: This feels like it's in the wrong place. Refactor eventually
+      $scope.startBattle = function() {
+        // Also determin power here too. 
+        // ML TODO: Make a service/factory for generating a monster and so it can keep track of it's health
+        switch($scope.difficulty) {
+          case 'easy':
+            $scope.monster.health = 100;
+            $scope.monster.power = 10;
+            $scope.monster.xp = 100;
+            break;
+          case 'medium':
+            $scope.monster.health = 200;
+            $scope.monster.power = 20;
+            $scope.monster.xp = 200;
+            break;
+          case 'hard':
+            $scope.monster.health = 300;
+            $scope.monster.power = 30;
+            $scope.monster.xp = 300; 
+            break;
+        }
+      };
+
+      $scope.restart = function() {
+        $scope.monster = {};
+        $scope.player.currentHealth = Global.user.maxHealth;
+        $scope.activityLog = [];
+        stateMachine.switchState($scope.state, 'start');
+        stateMachine.switchState($scope.battleState, 'question');
       };
     }
   ]).controller('Battle', ['$scope', '$rootScope', 'apiFetch', 'rng', 'Global', 'stateMachine', 
     function($scope, $rootScope, apiFetch, rng, Global, stateMachine) {
-      $scope.player = {
-        currentHealth: Global.user.maxHealth, // set at max for now
-        power: 40 // I'm not sure what the spec is for power at the moment
-      };
-
       $scope.problem = {
         question: 'temp'
-      };
-
-      $scope.monster = {
-        // these will be filled in
-        health: 0
-      };
-
-      $scope.battleState = {
-        'question' : true,
-        'postAnswer' : false,
-        'done': false
       };
 
       $scope.difficultyChances = {
@@ -53,33 +84,13 @@ angular.module('mean.battle')
         'hard': 75
       };
 
-      $scope.startBattle = function() {
-        // Also determin power here too. 
-        // ML TODO: Make a service for generating a monster and so it can keep track of it's health
-        switch($scope.difficulty) {
-          case 'easy':
-            $scope.monster.health = 100;
-            $scope.monster.power = 10;
-            break;
-          case 'medium':
-            $scope.monster.health = 200;
-            $scope.monster.power = 20;
-            break;
-          case 'hard':
-            $scope.monster.health = 300;
-            $scope.monster.power = 30;
-            break;
-        }
-        console.log("test")
-      };
-
       $scope.afterTurn = function(questionResult) {
         // this all changes with difficulty as well
         if(questionResult) {
           switch($scope.difficulty) {
             case 'easy':
               $scope.playerAttack();
-              $scope.nextProblem();
+              $scope.endOfRound();
               break;
             case 'medium':
               $scope.rollPlayerAction();
@@ -90,20 +101,11 @@ angular.module('mean.battle')
           }
         } else {
           $scope.enemyAttack();
-        }
-
-        if($scope.isSomeoneDead()) {
-          // we finished, figure out who died
-          if($scope.player.currentHealth <= 0) {
-            stateMachine.switchState($scope.state, 'failure');
-          } else { // monster is dead
-            stateMachine.switchState($scope.state, 'success');
-          }
+          $scope.endOfRound();
         }
       };
 
       $scope.fetchProblem = function() {
-        $scope.user_attempt = "";
         // change the difficulty here
         apiFetch.fetchProblem('easy', 'math', function(p) {
           $scope.problem = p;
@@ -121,14 +123,14 @@ angular.module('mean.battle')
         var attempt = $scope.user_attempt;
 
         apiFetch.submitAttempt($scope.problem.id, attempt, function(result) {
+          $scope.user_attempt = ''
           if(result.result) {
             $scope.addActivityLog('You answered correctly!');
-            stateMachine.switchState($scope.state, 'success');
+            // stateMachine.switchState($scope.state, 'success');
           } else {
-            $scope.result = "You answered incorrectly.";
-            stateMachine.switchState($scope.state, 'failure');
+            $scope.addActivityLog('You answered incorrectly.');
+            // stateMachine.switchState($scope.state, 'failure');
           }
-          $scope.attempted = true; // I feel like this should be in a state machine
           $scope.afterTurn(result.result);
         });
       };
@@ -138,20 +140,25 @@ angular.module('mean.battle')
 
         // just basic health minus power
         $scope.player.currentHealth -= $scope.monster.power;
+        $scope.addActivityLog('Monster dealt ' + $scope.monster.power + ' damage');
       };
 
       $scope.playerAttack = function(factor) {
         // Player does damage
-        factor = typeof(factor) === undefined ? factor : 1.0; // attempt at default parameters
+        factor = typeof(factor) === "undefined" ? 1.0 : factor; // attempt at default parameters
 
-        $scope.monster.health -= $scope.player.power * factor;
+        var damage = $scope.player.power * factor;
+        $scope.monster.health -= damage;
+
+        $scope.addActivityLog('You dealt ' + damage + ' damage');
       };
 
       $scope.isSomeoneDead = function() {
-        return $scope.monster.health <= 0 || $scope.player.currentHealth <= 0;
+        return ($scope.monster.health <= 0 || $scope.player.currentHealth <= 0);
       }
 
       $scope.rollPlayerAction = function() {
+
         if(rng.rollDice($scope.difficultyChances[$scope.difficulty])) {
           // roll succeded
           // the player now has a choice between two actions
@@ -159,6 +166,7 @@ angular.module('mean.battle')
         } else {
           // roll failed. just do basic attack
           $scope.playerAttack();
+          $scope.endOfRound();
         }
       };
 
@@ -169,6 +177,23 @@ angular.module('mean.battle')
           $scope.enemyAttack();
         } else { // choice === 'miss'
           $scope.playerAttack();
+        }
+
+        $scope.endOfRound();
+      };
+
+      $scope.endOfRound = function() {
+        if($scope.isSomeoneDead()) {
+          // we finished, figure out who died
+          if($scope.player.currentHealth <= 0) {
+            stateMachine.switchState($scope.state, 'failure');
+            $scope.addActivityLog('You lost...');
+          } else { // monster is dead
+            stateMachine.switchState($scope.state, 'success');
+            $scope.addActivityLog('You won!');
+          }
+        } else {
+          $scope.nextProblem();
         }
       };
     }
